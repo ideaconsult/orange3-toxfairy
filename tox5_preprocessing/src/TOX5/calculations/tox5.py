c2f17e9a39de6e4919b48aeea5c60d95619b0686
GerganaTancheva123 <gerganatancheva1@gmail.com>
2023-07-26 14:47:31 +0300

    install/import r packages functionallity and yeo-johnson transform

--- tox5_preprocessing/src/TOX5/calculations/tox5.py
+++ tox5_preprocessing/src/TOX5/calculations/tox5.py
@@ -1,21 +1,40 @@
 import pandas as pd
 from rpy2 import robjects
-from rpy2.robjects.packages import importr, data
+from rpy2.robjects.packages import importr, isinstalled
 from rpy2.robjects import pandas2ri
 import rpy2.robjects as ro
 from rpy2.robjects.vectors import ListVector
-
 from itertools import product
 import re
-
-# import warnings
-# warnings.filterwarnings('ignore')
-# utils.install_packages('toxpiR')
+import warnings
+warnings.filterwarnings('ignore')
 
 base = importr("base")
 utils = importr("utils")
 utils.chooseCRANmirror(ind=1)
-toxpiR = importr("toxpiR")
+
+
+def get_source_package(package_name):
+    if isinstalled(package_name):
+        source_package = importr(package_name)
+        return source_package
+    else:
+        utils.install_packages(package_name)
+        source_package = importr(package_name)
+        return source_package
+
+
+car = get_source_package('car')
+toxpiR = get_source_package('toxpiR')
+
+# def extract_versions(package_data):
+#     return dict(zip(
+#         package_data.rx(True, 'Package'),
+#         package_data.rx(True, 'Version')
+#     ))
+# i = extract_versions(utils.installed_packages())
+# a = extract_versions(utils.available_packages())
+# is_latest_version = i['car'] == a['car']
 
 
 class TOX5:
@@ -25,6 +44,7 @@ class TOX5:
         self.manual_names = manual_names
         self.manual_slices = manual_slices
 
+        self.__transformed_data_r = []
         self.__transformed_data = []
         self.__tox5_scores = pd.DataFrame()
         self.__all_slice_names = list((self.data.loc[:, self.data.columns.str.startswith(tuple(self.cell))]).keys())
@@ -32,6 +52,14 @@ class TOX5:
     @property
     def all_slice_names(self):
         return self.__all_slice_names
+
+    @property
+    def transformed_data_r(self):
+        return self.__transformed_data_r
+
+    @transformed_data_r.setter
+    def transformed_data_r(self, value):
+        self.__transformed_data_r = value
 
     @property
     def transformed_data(self):
@@ -49,15 +77,27 @@ class TOX5:
     def tox5_scores(self, value):
         self.__tox5_scores = value
 
-    def transform_data(self):
+    def transform_data(self, user_transform_funcs):
         with ro.default_converter + pandas2ri.converter:
             r_from_pd_df = ro.conversion.get_conversion().py2rpy(self.data)
 
         slice_names_ = robjects.StrVector(self.all_slice_names)
+        user_transform_funcs_r = robjects.ListVector(user_transform_funcs)
 
         transforming_data = robjects.r('''
-            transforming_data <- function(slice_names_, df){
-                    trans_func <- TxpTransFuncList(tf1 = function(x) sqrt(x), tf2 = function(x) -log10(x)+6)
+            transforming_data <- function(slice_names_, df, tf_dict){
+                    yeo_johnson_wrapper <- function(x) {
+                      # Estimate the optimal lambda using powerTransform()
+                      yeo_transform <- powerTransform(x, family = "yjPower")
+                      lambda_est <- yeo_transform$yjlambda
+                      # Apply the Yeo-Johnson transformation using the estimated lambda
+                      transformed_data <- yjPower(x, yeo_transform$lambda, jacobian.adjusted = FALSE)
+                    }
+                    
+                    tf1 <- TxpTransFunc(function(x) sqrt(x))
+                    tf2 <- TxpTransFunc(function(x) -log10(x)+6)
+                    tf3 <- TxpTransFunc(yeo_johnson_wrapper)
+                    trans_func <- TxpTransFuncList(sqrt_x = tf1, log10x_6 = tf2, yeo_johnson = tf3)
 
                     test_slice <- list()
                     slice_names <- c()
@@ -66,11 +106,11 @@ class TOX5:
                         slice_names <- c(slice_names, x)
 
                         if (grepl("1st",x,ignore.case=TRUE)){
-                            test_slice <- append(test_slice, x = TxpSlice(x, trans_func[rep("tf2", 1)]))
+                            test_slice <- append(test_slice, x = TxpSlice(x, trans_func[rep(tf_dict[["1st"]], 1)]))
                         }else if (grepl("auc",x,ignore.case=TRUE)){
-                            test_slice <- append(test_slice, x = TxpSlice(x, trans_func[rep("tf1", 1)]))
+                            test_slice <- append(test_slice, x = TxpSlice(x, trans_func[rep(tf_dict[["auc"]], 1)]))
                         } else if (grepl("max",x,ignore.case=TRUE)){
-                            test_slice <- append(test_slice, x = TxpSlice(x, trans_func[rep("tf2", 1)]))
+                            test_slice <- append(test_slice, x = TxpSlice(x, trans_func[rep(tf_dict[["max"]], 1)]))
                         }
                     }
 
@@ -81,7 +121,10 @@ class TOX5:
                     return (as.data.frame((sort(results)), id.name = "Material", score.name = "toxpi_score", rank.name = "rnk"))
             }
         ''')
-        self.transformed_data = transforming_data(slice_names_, r_from_pd_df)
+        self.transformed_data_r = transforming_data(slice_names_, r_from_pd_df, user_transform_funcs_r)
+
+        with ro.default_converter + pandas2ri.converter:
+            self.transformed_data = ro.conversion.get_conversion().rpy2py(self.transformed_data_r)
 
     def generate_auto_slices(self, slicing_pattern='by_time_endpoint'):
         cell = set([item.split('_')[0] for item in self.all_slice_names])
@@ -143,7 +186,7 @@ class TOX5:
                     }
                 ''')
 
-        result = calculate_scores(self.transformed_data, slices_names_r, slices_r)
+        result = calculate_scores(self.transformed_data_r, slices_names_r, slices_r)
 
         with ro.default_converter + pandas2ri.converter:
             self.tox5_scores = ro.conversion.get_conversion().rpy2py(result)
