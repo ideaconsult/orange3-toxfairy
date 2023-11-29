5d4bf5b07afb5fec79a3a9902b355aba702ebd68
GerganaTancheva123 <gerganatancheva1@gmail.com>
2023-11-29 14:34:18 +0200

    save widget state in .ows; add error,warning msg; update drop-down menues; correct some btns labels

--- tox_orange_demo/ow_read_data.py
+++ tox_orange_demo/ow_read_data.py
@@ -1,14 +1,14 @@
-import os
-import glob
 import re
 import pandas as pd
 import copy
 from PyQt5.QtWidgets import QTableWidget, QTableWidgetItem
 from Orange.data import table_from_frame
-from Orange.widgets.widget import OWWidget, Input, Output
+from Orange.widgets.widget import OWWidget, Input, Output, Msg
 from Orange.widgets import gui
 import Orange.data
+from Orange.widgets.settings import Setting
 from AnyQt.QtWidgets import QFileDialog
+from orangewidget import widget
 
 from TOX5.endpoints.hts_data_container import HTS
 from TOX5.endpoints.reader_from_tmp import DataReaderTmp, MetaDataReaderTmp
@@ -20,19 +20,33 @@ class Toxpi(OWWidget):
     description = "Read HTS data and annotate with meta data"
     icon = "icons/print.svg"
 
+    UserAdviceMessages = [
+        widget.Message("Please enter endpoints separately using either a dot and whitespace (. ) or a comma and "
+                       "whitespace (, ). Available endpoints: CTG, CASP, DAPI, H2AX, OHG", '')]
+
     class Inputs:
-        data_input = Input("Directory to data ", Orange.data.Table)
-        meta_data_input = Input("File for meta data", Orange.data.Table)
+        data_input = Input("Directory to data ", Orange.data.Table, default=True)
+        meta_data_input = Input("File for meta data", Orange.data.Table, default=False)
 
     class Outputs:
         data_dict = Output("Data dictionary", dict)
+        meta_data_table = Output("Meta data", Orange.data.Table)
+
+    endpoint = Setting('', schema_only=True)
+    recalculate = Setting(True, schema_only=True)
+    well_volume = Setting("50", schema_only=True)
+    cell_growth_area = Setting("0.079495092", schema_only=True)
+    radioBtnSelection = Setting(0, schema_only=True)
+
+    class Warning(OWWidget.Warning):
+        warning_func = Msg("Function is under development, please select another one.")
+
+    class Error(OWWidget.Error):
+        no_datafiles_error = Msg("Please, load files with data")
 
     def __init__(self):
         super().__init__()
         self.endpoint_view = ''
-
-        self.data = None
-        self.meta_data = None
         self.annot_file = None
         self.data_files_path = None
 
@@ -43,136 +57,159 @@ class Toxpi(OWWidget):
         self.previous_table = None
         self.raw_data = pd.DataFrame()
 
-        # control area with reading box, directory box and recalculation dose box //////////////////////////////////////
+        """
+        Control area with entering endpoints box, reading data and meta data box from selected dirs, 
+        and recalculation dose box.
+        """
 
-        # box to enter endpoints ---------------------------------------------------------------------------------------
         self.box_read = gui.widgetBox(self.controlArea, 'Read and annotate data from local directory')
-        self.endpoint = gui.lineEdit(self.box_read, self, '', label='Enter endpoints:', callback=self.endpoint_names)
-        # self.endpoint.setFixedWidth(100)
-
-        # box to set proper dir with data for each endpoint ------------------------------------------------------------
+        self.endpoint_line = gui.lineEdit(self.box_read, self, 'endpoint', label='Enter endpoints:',
+                                          callback=self.endpoint_names)
         self.box_dir = gui.widgetBox(self.controlArea, 'Set proper dir')
-
-        # box for annotation file --------------------------------------------------------------------------------------
         self.box_annot = gui.widgetBox(self.controlArea, 'Set proper annotation dir')
         self.annot_dir = gui.comboBox(self.box_annot, self, 'annot_data', label='Annotation file',
                                       sendSelectedValue=True)
-
-        # box to recalculate doses -------------------------------------------------------------------------------------
         self.box_calc = gui.widgetBox(self.controlArea, 'Recalculate dose')
-        self.recalculate = False
         self.toggle_button = gui.checkBox(self.box_calc, self, "recalculate", "Recalculate Dose",
                                           callback=self.toggle_recalculation)
-        self.well_volume_label = gui.label(None, self, 'Enter well volume in ul:')
-        self.well_volume = gui.lineEdit(None, self, '', label='Enter well volume in ul')
-        self.cell_growth_area_label = gui.label(None, self, 'Enter cell growth area in ul:')
-        self.cell_growth_area = gui.lineEdit(None, self, '', label='Enter cell growth area in ul')
-        self.radioBtnSelection = None
+        self.well_volume_label = gui.label(None, self, 'Enter well volume in μl:')
+        self.well_volume_line = gui.lineEdit(None, self, 'well_volume', label='ul')
+        self.cell_growth_area_label = gui.label(None, self, 'Enter plate growth area  in cm2:')
+        self.cell_growth_area_line = gui.lineEdit(None, self, 'cell_growth_area', label='cm2')
         self.radioBtns = gui.radioButtonsInBox(None, self, 'radioBtnSelection',
-                                               btnLabels=['recalculate_dose_from_cell_growth',
-                                                          'recalculate_dose_from_sbet',
-                                                          'recalculate_dose_from_cell_delivered_dose'],
-                                               tooltips=['Use this option for automated slicing by time-endpoint',
-                                                         'Use this option for automated slicing by endpoint',
-                                                         'Use this option for manually slicing'])
+                                               btnLabels=['Dose per Cell Area (μg/cm²)',
+                                                          'Dose based on Surface Area (cm²/cm²)',
+                                                          'Cell Delivered Dose (cell delivered cm²/cm²)'],
+                                               tooltips=['Nominal dose per cell cell growth area (μg/cm2)',
+                                                         'Nominal dose based on the material’s specific surface area '
+                                                         'per cell growth area',
+                                                         'The cell delivered dose per cell growth area '])
         gui.button(self.controlArea, self, 'Process', callback=self.process, autoDefault=False)
 
-        # main area with choosen data view and save as a file //////////////////////////////////////////////////////////
+        """
+        Main area with data view of chosen endpoint.
+        """
         self.mainBox = gui.widgetBox(self.mainArea, 'Data view')
         self.combo_box_main = gui.comboBox(None, self, 'endpoint_view', label='Select endpoint',
                                            sendSelectedValue=True, callback=self.view)
         self.view_table = gui.widgetBox(None, 'table')
         self.saveBtn = gui.button(None, self, 'Save as', callback=self.save_table, autoDefault=False)
 
+        self.toggle_recalculation()
+
     @Inputs.data_input
     def set_pat(self, data):
         if data:
-            self.data = data
-            self.data_files_path = self.data.metas
+            self.data_files_path = data.metas
+            self.Error.no_datafiles_error.clear()
+            self.endpoint_names()
         else:
-            self.data = None
+            self.data_files_path = None
 
     @Inputs.meta_data_input
     def set_file(self, meta_data):
         if meta_data:
-            self.meta_data = meta_data
-            self.annot_file = self.meta_data.metas
+            self.annot_file = meta_data.metas
+            self.Error.no_datafiles_error.clear()
+            self.endpoint_names()
         else:
             self.annot_file = None
 
     def toggle_recalculation(self):
         if self.recalculate:
             self.box_calc.layout().addWidget(self.well_volume_label)
-            self.box_calc.layout().addWidget(self.well_volume)
+            self.box_calc.layout().addWidget(self.well_volume_line)
             self.box_calc.layout().addWidget(self.cell_growth_area_label)
-            self.box_calc.layout().addWidget(self.cell_growth_area)
+            self.box_calc.layout().addWidget(self.cell_growth_area_line)
             self.box_calc.layout().addWidget(self.radioBtns)
         else:
             self.well_volume_label.setParent(None)
             self.cell_growth_area_label.setParent(None)
-            self.well_volume.setParent(None)
-            self.cell_growth_area.setParent(None)
+            self.well_volume_line.setParent(None)
+            self.cell_growth_area_line.setParent(None)
             self.radioBtns.setParent(None)
 
     def endpoint_names(self):
-        self.endpoints_list = []
-        self.endpoints_list = re.split(r'[,.]\s+', self.endpoint.text())
-        self.endpoints_list = [x.upper() for x in self.endpoints_list]
+        if self.endpoint_line.text():
+            self.endpoints_list = []
+            self.endpoints_list = re.split(r'[,.]\s+', self.endpoint_line.text())
+            self.endpoints_list = [x.upper() for x in self.endpoints_list]
+            self.remove_existing_widgets()
 
-        if all(item == '' for item in self.endpoints_list):
-            self.remove_existing_widgets()
-        else:
-            self.remove_existing_widgets()
+        if all(item != '' for item in self.endpoints_list):
             self.chose_dir()
 
     def chose_dir(self):
-        self.selected_values_dict = {}
-        meta_values = [item[0] for item in self.meta_data.metas]
-        data_values = [item[0] for item in self.data.metas]
-        test = meta_values + data_values
+        if self.annot_file is None or self.data_files_path is None or not self.annot_file.any() or not self.data_files_path.any():
+            self.Error.no_datafiles_error()
+        else:
+            self.selected_values_dict = {}
+            meta_values = [item[0] for item in self.annot_file]
+            data_values = [item[0] for item in self.data_files_path]
 
-        for i, items in enumerate(self.endpoints_list):
-            combo_box = gui.comboBox(self.box_dir, self, '', label=f'{items}', items=test)
-            combo_box.activated.connect(lambda value, items=items: self.save_selected_value(items, test[value]))
+            for i, items in enumerate(self.endpoints_list):
+                combo_box = gui.comboBox(self.box_dir, self, '', label=f'{items}', items=data_values)
+                combo_box.activated.connect(
+                    lambda value, items=items: self.save_selected_value(items, data_values, value))
 
-        self.annot_dir.clear()
-        self.annot_dir.addItems(test)
+                if data_values:
+                    combo_box.setCurrentText(data_values[0])
+                    self.save_selected_value(items, data_values, 0)
 
-    def save_selected_value(self, items, value):
-        self.selected_values_dict[items] = value
+            self.annot_dir.clear()
+            self.annot_dir.addItems(meta_values)
+
+            if meta_values:
+                self.annot_data = meta_values[0]
+
+    def save_selected_value(self, items, values, index):
+        if index < len(values):
+            self.selected_values_dict[items] = values[index]
+        else:
+            print(f"Index {index} out of range for values {values}")
 
     def remove_existing_widgets(self):
         for i in reversed(range(self.box_dir.layout().count())):
             self.box_dir.layout().itemAt(i).widget().deleteLater()
 
     def process(self):
+        self.output_dict = {}
+        meta_data_dict = {}
         for key, value in self.selected_values_dict.items():
-            print(key, value)
             test_data = HTS(key)
             test_meta = MetaDataReaderTmp(self.annot_data, test_data)
             test_meta.read_meta_data()
             test_read = DataReaderTmp(self.annot_data, value, test_data)
             test_read.read_data()
 
-            if self.radioBtnSelection == 0:
-                test_meta.recalculate_dose_from_cell_growth(float(self.well_volume.text()),
-                                                            float(self.cell_growth_area.text()))
-            elif self.radioBtnSelection == 1:
-                test_meta.recalculate_dose_from_sbet(float(self.well_volume.text()),
-                                                     float(self.cell_growth_area.text()))
-            elif self.radioBtnSelection == 2:
-                print('func is not implemented')
+            if self.recalculate:
 
-            if key not in self.output_dict:
-                self.output_dict[key] = []
-            self.output_dict[key].append(test_data)
+                if self.radioBtnSelection == 0:
+                    self.Warning.warning_func.clear()
+                    test_meta.recalculate_dose_from_cell_growth(float(self.well_volume_line.text()),
+                                                                float(self.cell_growth_area_line.text()))
+                elif self.radioBtnSelection == 1:
+                    self.Warning.warning_func.clear()
+                    test_meta.recalculate_dose_from_sbet(float(self.well_volume_line.text()),
+                                                         float(self.cell_growth_area_line.text()))
+                elif self.radioBtnSelection == 2:
+                    self.Warning.warning_func()
+
+            self.output_dict[key] = [test_data]
+            meta_data_dict = test_data.metadata
+
+        df_meta_data = pd.DataFrame(meta_data_dict)
 
         self.combo_box_main.clear()
         self.combo_box_main.addItems(self.endpoints_list)
+        self.endpoint_view = self.endpoints_list[0]
         self.mainBox.layout().addWidget(self.combo_box_main)
         self.mainBox.layout().addWidget(self.view_table)
 
+        self.view()
+
         self.Outputs.data_dict.send(self.output_dict)
+        self.Outputs.meta_data_table.send(table_from_frame(df_meta_data))
 
     def view(self):
         output_dict_copy = copy.deepcopy(self.output_dict)
@@ -187,7 +224,7 @@ class Toxpi(OWWidget):
         else:
             print('key doesnt exist')
 
-        table_widget = QTableWidget()
+        table_widget = QTableWidget(self.view_table)
         num_rows, num_cols = self.raw_data.shape
         table_widget.setRowCount(num_rows)
         table_widget.setColumnCount(num_cols)
@@ -207,7 +244,6 @@ class Toxpi(OWWidget):
         file_path, _ = QFileDialog.getSaveFileName(self, "Save Table", "",
                                                    "CSV Files (*.csv);;Excel Files (*.xlsx);;All Files (*)",
                                                    options=options)
-
         if file_path:
             if file_path.endswith(".csv"):
                 self.raw_data.to_csv(file_path)
