3cfc6bbbf0022fed92e84be29ed91e3cc2f0ea8a
GerganaTancheva123 <gerganatancheva1@gmail.com>
2024-05-22 16:24:51 +0300

    Update pie view and bootstrap CIs

--- tox5_preprocessing/src/TOX5/misc/utils.py
+++ tox5_preprocessing/src/TOX5/misc/utils.py
@@ -3,12 +3,14 @@ import os
 import glob
 import warnings
 import numpy as np
-from matplotlib import cm
+from matplotlib import cm, patches
 import matplotlib.pyplot as plt
 import math
 
-
 # TODO: to be removed
+from matplotlib.colors import ListedColormap
+
+
 def add_annot_data(df, material, concentration, code):
     return df.append(pd.Series(material, index=df.columns, name='material')) \
         .append(pd.Series(concentration, index=df.columns, name='concentration')) \
@@ -44,7 +46,6 @@ def _extract_info_from_filename(file_name):
 
 
 def generate_annotation_file(directories, template_path):
-
     if isinstance(directories, str):
         directories = [directories]
 
@@ -79,7 +80,52 @@ def generate_annotation_file(directories, template_path):
         warnings.warn(error_message, Warning)
 
 
-def plot_tox_rank_pie(df, materials=None, figure=None):
+def _create_color_map(labels, colored_by='endpoint'):
+    """
+    Parameters:
+    - colored_by (string): the strings could be "endpoint", "cells", "time" and pies will be separately
+    colored by chosen parameter.
+
+    """
+    cmap_galery = ['Purples', 'Greens', 'Blues', 'Oranges', 'Reds', 'Greys',
+                   'YlOrBr', 'YlOrRd', 'OrRd', 'PuRd', 'RdPu', 'BuPu',
+                   'GnBu', 'PuBu', 'YlGnBu', 'PuBuGn', 'BuGn', 'YlGn']
+
+    cells_set = set()
+    times_set = set()
+    endpoints_set = set()
+    for label in labels:
+        cell, time, endpoint = label.split('_')
+        cells_set.add(cell)
+        times_set.add(time)
+        endpoints_set.add(endpoint)
+
+    color_palette_dict = {}
+    if colored_by == 'cells':
+        selected_set = cells_set
+    elif colored_by == 'time':
+        selected_set = times_set
+    elif colored_by == 'endpoint':
+        selected_set = endpoints_set
+    else:
+        raise ValueError("Invalid value for colored_by. Choose from 'endpoint', 'cells', 'time'.")
+
+    for idx, param in enumerate(selected_set):
+        num_occurrences = sum(param in label for label in labels)
+        colors_pallete = plt.colormaps[cmap_galery[idx]](np.linspace(0.2, 1, num_occurrences)).tolist()
+        for i, label in enumerate(labels):
+            if param in label:
+                color = colors_pallete.pop(0)
+                color_palette_dict[label] = color
+            else:
+                continue
+
+    colors = [color_palette_dict[label] for label in labels]
+    return colors
+
+
+def plot_tox_rank_pie(df, materials=None, figure=None, colored_param='cells', transparency_bars=0.6, linewidth=0.2,
+                      conf_intervals=None, ci_low_color="#ff5c33", ci_high_color="#ff5c33", pies_per_col=2):
     """
     Plot Tox5-score materials using a pie plot.
 
@@ -93,20 +139,11 @@ def plot_tox_rank_pie(df, materials=None, figure=None):
     - materials (list or None, optional): List of materials to plot. If None, all unique materials in the DataFrame are used.
     - figure (matplotlib.figure.Figure or None, optional): The Figure object to use for the plot. If None, a new Figure is created.
       Used in Orange canvas to plot.
+    - conf_intervals (dict or None, optional): Dictionary containing confidence intervals for each slice.
+      Keys are the slice labels, values are tuples (CI_lower, CI_upper).
 
     Returns:
     - tuple of matplotlib.figure.Figure: The main figure with pie view of each material and a separate figure for the legend.
-
-    Example:
-    ```
-    # Plot Tox5-score materials with default options
-    main_fig, legend_fig = plot_tox_rank_pie(df)
-
-    # Optionally specify materials and existing Figure
-    selected_materials = ['Material1', 'Material2']
-    existing_figure = plt.figure()
-    main_fig, legend_fig = plot_tox_rank_pie(df, materials=selected_materials, figure=existing_figure)
-    ```
     """
     if figure is None:
         figure = plt.figure(tight_layout=True)
@@ -115,38 +152,67 @@ def plot_tox_rank_pie(df, materials=None, figure=None):
     plt.subplots_adjust(hspace=0.2)
     plt.suptitle("Toxpi Ranked materials", fontsize=10, y=0.95)
 
+    if 'low_ci' in df.columns:
+        df = df.drop(columns=['low_ci'])
+
+    if 'high_ci' in df.columns:
+        df = df.drop(columns=['high_ci'])
+
     if materials is None:
         materials = df['Material'].unique().tolist()
 
-    columns = math.ceil(len(materials) / 5)
-
+    columns = math.ceil(len(materials) / pies_per_col)  # /5
     rows = len(materials) // columns + (len(materials) % columns > 0)
 
+    labels = df.columns[3:].tolist()
+
+    # Sort labels based on the chosen parameter
+    labels_sorted = sorted(labels, key=lambda x: x.split('_')[{'cells': 0, 'time': 1, 'endpoint': 2}[colored_param]])
+
+    # Create colormap
+    colors = _create_color_map(labels_sorted, colored_by=colored_param)
     legend_handles = []
-    labels = df.columns[4:].tolist()
 
     for n, i in enumerate(materials):
         rank = (df[df['Material'] == i].iloc[:, 2]).values[0]
-        data_tox = df[df['Material'] == i].iloc[:, 4:].values.flatten().tolist()
-        width = 2 * np.pi / len(labels)
-        angle = np.linspace(0.0, 2 * np.pi, len(labels), endpoint=False)
-        colors = cm.get_cmap('plasma', len(labels)).colors
+        data_tox = df[df['Material'] == i].iloc[:, 3:].values.flatten().tolist()
+
+        # Sort data_tox based on the sorted labels
+        data_tox_sorted = [data_tox[labels.index(label)] for label in labels_sorted]
+
+        width = 2 * np.pi / len(labels_sorted)
+        angle = np.linspace(0.0, 2 * np.pi, len(labels_sorted), endpoint=False)
 
         ax = plt.subplot(rows, columns, n + 1, projection='polar')
-        bars = ax.bar(angle, data_tox, width=width, bottom=0.02, color=colors, alpha=0.6, edgecolor='grey')
+        bars = ax.bar(angle, data_tox_sorted, width=width, bottom=0.02, color=colors, alpha=transparency_bars,
+                      edgecolor='grey', linewidth=linewidth)
+
+        # Plot confidence intervals if provided
+        if conf_intervals:
+            for j, label in enumerate(labels_sorted):
+                if label in conf_intervals[i]:
+                    conf_interval = conf_intervals[i][label]
+                    ax.fill_between([angle[j] - width / 2, angle[j] + width / 2], conf_interval[0], conf_interval[1],
+                                    color='gray', alpha=0.1)
+
+                    ax.plot([angle[j] - width / 2, angle[j] + width / 2], [conf_interval[0], conf_interval[0]],
+                            color=ci_low_color, linestyle='-', linewidth=1)
+                    ax.plot([angle[j] - width / 2, angle[j] + width / 2], [conf_interval[1], conf_interval[1]],
+                            color=ci_high_color, linestyle='-', linewidth=1)
+
         ax.set_ylim(0, 1)
         ax.set_xticks([])
-        ax.set_yticks([])
-        ax.grid(False)
+        ax.set_yticks([0.2, 0.5, 1.0])
+        # ax.grid(False)
         ax.spines['polar'].set_color('grey')
         plt.title(f"{i} {rank.round(2)}", fontsize=8)
 
         legend_handles.extend(bars)
 
     legend_figure = plt.figure()
-    legend_handles = legend_handles[len(labels):]
+    legend_handles = legend_handles[:len(labels_sorted)]
 
-    plt.legend(legend_handles, labels, loc='center', fontsize='small', ncol=4, mode='expand')
+    plt.legend(legend_handles, labels_sorted, loc='center', fontsize='small', ncol=4, mode='expand')
     plt.axis('off')
 
     return figure, legend_figure
