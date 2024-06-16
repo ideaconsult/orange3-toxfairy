33697a2572542c036b086b66356a2d0364533f94
GerganaTancheva123 <gerganatancheva1@gmail.com>
2024-06-16 19:25:17 +0300

    added plotly as a required lib

--- setup.py
+++ setup.py
@@ -11,12 +11,12 @@ INSTALL_REQUIRES = [
     'orange3~=3.0',
     'orange_widget_base',
     'pandas',
-    # 'pandas',
     'PyQt5',
     'rpy2',
     'scikit_learn',
     'scipy',
-    'setuptools'
+    'setuptools',
+    'plotly'
 ]
 
 PACKAGES = ["tox_orange_demo"]
