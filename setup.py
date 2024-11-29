12a5b22f500424d9690c7ec7c75dfe1fa140f9c2
Luchesar ILIEV <luchesar.iliev@gmail.com>
2024-11-29 16:53:39 +0200

    rm versions

--- setup.py
+++ setup.py
@@ -7,19 +7,19 @@ VERSION = '1'
 AUTHOR = 'IDEAconsult Ltd.'
 
 INSTALL_REQUIRES = [
-    'AnyQt~=0.2.0',
-    'PyQt5~=5.15.0',
-    'matplotlib~=3.9.0',
-    'numpy~=1.26.0',
-    'orange3~=3.38.0',
-    'orange_widget_base~=4.25.0',
-    'pandas~=2.2.0',
-    'plotly~=5.24.1',
-    'rpy2~=3.5.0',
-    'scikit_learn~=1.5.0',
-    'scipy~=1.14.0',
-    'setuptools~=75.6.0',
-    'toxfairy~=0.1.0',
+    'AnyQt',
+    'PyQt5',
+    'matplotlib',
+    'numpy',
+    'orange3',
+    'orange_widget_base',
+    'pandas',
+    'plotly',
+    'rpy2',
+    'scikit_learn',
+    'scipy',
+    'setuptools',
+    'toxfairy',
 ]
 
 PACKAGES = ["orange3_toxfairy"]
--- toxfairy/setup.py
+++ toxfairy/setup.py
@@ -7,15 +7,15 @@ VERSION = '0.1.0'
 AUTHOR = 'IDEAconsult Ltd.'
 
 INSTALL_REQUIRES = [
-    'matplotlib~=3.9.0',
-    'numpy~=1.26.0',
-    'pandas~=2.2.0',
-    'pymcdm~=1.2.0',
-    'pynanomapper~=2.0.1',
-    'rpy2~=3.5.0',
-    'scikit_learn~=1.5.2',
-    'scipy~=1.14.0',
-    'setuptools~=75.6.0',
+    'matplotlib',
+    'numpy',
+    'pandas',
+    'pymcdm',
+    'pynanomapper',
+    'rpy2',
+    'scikit_learn',
+    'scipy',
+    'setuptools',
 ]
 
 PACKAGES = find_packages(where='src')
