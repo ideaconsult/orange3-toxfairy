1a025c6943f9f9aad5629f120c587db1a5befca8
Luchesar ILIEV <luchesar.iliev@gmail.com>
2024-12-05 15:50:21 +0200

    ops: pin more compatible deps versions

--- setup.py
+++ setup.py
@@ -7,19 +7,19 @@ VERSION = '1'
 AUTHOR = 'IDEAconsult Ltd.'
 
 INSTALL_REQUIRES = [
-    'AnyQt',
-    'PyQt5',
-    'matplotlib',
-    'numpy',
-    'orange3',
-    'orange_widget_base',
-    'pandas',
-    'plotly',
-    'rpy2',
-    'scikit_learn',
-    'scipy',
-    'setuptools',
-    'toxfairy',
+    'AnyQt~=0.2.0',
+    'PyQt5~=5.15.0',
+    'matplotlib~=3.9.0',
+    'numpy~=1.26.0',
+    'orange3~=3.38',
+    'orange_widget_base~=4.25',
+    'pandas~=2.2.0',
+    'plotly~=5.24.0',
+    'rpy2~=3.5.0',
+    'scikit_learn~=1.5.0',
+    'scipy~=1.14.0',
+    'setuptools~=75.6.0',
+    'toxfairy~=0.1.0',
 ]
 
 PACKAGES = ["orange3_toxfairy"]
